1282bba1a3a4d48570b858d9738ab63af5522ca8
peppersora <110614387+peppersora@users.noreply.github.com>
2023-01-23 23:19:33 +0900

    firt commit

---
+++ src/theme.ts
@@ -0,0 +1,16 @@
+/* styled 파일에 정의된 속성들과 똑같아야함
+ */
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme:DefaultTheme = {
+    textColor: "#f5f6fa",
+    bgColor: "black",
+    accentColor: "#fbc531"
+}
+
+export const lightTheme:DefaultTheme = {
+    textColor: "f5f6fa",
+    bgColor: "whitesmoke",
+    accentColor: "#fbc531"
+}
+
