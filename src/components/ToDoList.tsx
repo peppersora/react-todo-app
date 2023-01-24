7bb6ea4915fade7c8ea43ca754339b1ab5001c4f
peppersora <110614387+peppersora@users.noreply.github.com>
2023-01-24 23:49:25 +0900

    selector

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,40 +1,34 @@
-
+import React from "react";
 import { useRecoilState, useRecoilValue} from "recoil";
-import { toDoSelector, toDoState } from "../atoms";
+import { categoryState, toDoSelector } from "../atoms";
 import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
 
 function ToDoList(){
    
-    const [toDo,doing,done] = useRecoilValue(toDoSelector);
-    
+    const toDos  = useRecoilValue(toDoSelector);
+    // useRecoilValue는 atom과 selector의 값만 반환한다.
+    const [category, setCategory] = useRecoilState(categoryState);
+    // useRecoilState는 값과 더불어 modifier함수도 제공
+    const onInput =(event:React.FormEvent<HTMLSelectElement>) => 
+    { setCategory(event.currentTarget.value)};
+    console.log(category);
+
     return(
         <div>
             <h1>To Dos</h1>
             <hr/>
-                <CreateToDo/>
-                <h2>To Do</h2>
-               <ul>
-                {toDo.map((toDo) =>(
-                <ToDo key={toDo.id}{...toDo}/>
-                ))}
-               </ul>
-               <hr/>
-               <h2>Doing</h2>
-               <ul>
-                {doing.map((toDo) =>(
-                <ToDo key={toDo.id}{...toDo}/>
-                ))}
-               </ul>
-               <hr/>
-               <h2>Done</h2>
-               <ul>
-                {done.map((toDo) =>(
-                <ToDo key={toDo.id}{...toDo}/>
-                ))}
-               </ul>
-               <hr/>
-               
+            
+            <select value={category} onInput={onInput}>
+                <option value="TO_DO">To Do</option>
+                <option value="DOING">Doing</option>
+                <option value="DONE">Done</option>
+            </select>
+            
+                <CreateToDo/>        
+                {toDos?.map((toDo) => (
+                    <ToDo key={toDo.id} {...toDo}/>
+                ))}    
                 </div>
     );
 }
